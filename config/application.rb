eec62b04ce715ddcf11fe35c2cf7751d00a18709
ZackCipolletta <ZackCipolletta@gmail.com>
2015-04-19 21:57:24 +0000

    changed fonts and added header etc...

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Beer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+
+    # precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .tff )
   end
 end
